40244e092b423456f5051dee1cc73fc120f839b1
Oliver Caldwell <olliec87@gmail.com>
2015-12-03 15:56:33 +0000

    Day 1 solution cleanup

--- days/day-1/src/day_1/core.clj
+++ days/day-1/src/day_1/core.clj
@@ -1,10 +1,20 @@
 (ns day-1.core)
 
-(defn find-floor [instructions]
-  (reduce +
-          (remove nil?
-                  (map {\( 1
-                        \) -1} instructions))))
+(def instruction-values
+  {\( 1
+   \) -1
+   \newline 0})
+
+(defn parse-instructions [instructions]
+  (map instruction-values (slurp "./input.txt")))
+
+(defn add-to-acc [acc x]
+  (conj acc (+ x (last acc))))
+
+(defn get-floors [instructions]
+  (reduce add-to-acc [0] (parse-instructions instructions)))
 
 (defn -main []
-  (println (find-floor (slurp "./input.txt"))))
+  (let [floors (get-floors (slurp "./input.txt"))]
+    (println "Last floor: " (last floors))
+    (println "First time at -1: " (+ 1 (.indexOf floors -1)))))
