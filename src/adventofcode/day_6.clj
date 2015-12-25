67d3bc9f4a1814713fd1d36dbd909cc68bef8070
Oliver Caldwell <olliec87@gmail.com>
2015-12-25 21:28:57 +0000

    Add tests for rule parsing

--- src/adventofcode/day_6.clj
+++ src/adventofcode/day_6.clj
@@ -1,6 +1,17 @@
-(ns adventofcode.day-6)
+(ns adventofcode.day-6
+  (:require [clojure.string :as string]))
 
 (def size 1000)
+
+(def state-to-bool {:on 1
+                    :off 0})
+
+(def str-to-state {"on" :on
+                   "off" :off
+                   "toggle" :toggle})
+
+(def invert-state {:on :off
+                   :off :on})
 
 (defn xy->i [x y]
   (+ x (* size y)))
@@ -9,12 +20,24 @@
   (get grid (xy->i x y) :off))
 
 (defn set-light [grid x y state]
-  (assoc grid (xy->i x y) state))
+  (if (= state :toggle)
+    (set-light grid x y (invert-state (get-light grid x y)))
+    (assoc grid (xy->i x y) state)))
 
-(defn apply-rule [lights rule]
-  lights)
+(defn parse-rule [rule-str]
+  (let [raw (string/split rule-str #" ")
+        parts (if (= "turn" (first raw))
+                (rest raw)
+                raw)]
+    {:from nil
+     :to nil
+     :state (str-to-state (first parts))}))
+
+(defn apply-rule [lights rule-str]
+  (let [rule (parse-rule rule-str)]
+    lights))
 
 (defn count-enabled-lights [input]
   (let [lights (reduce apply-rule {} input)
-        light-nums (map {:on 1 :off 0} lights)]
+        light-nums (map state-to-bool lights)]
     (reduce-kv (fn [acc k v] (+ acc v)) 0 lights)))
--- test/adventofcode/day_6_test.clj
+++ test/adventofcode/day_6_test.clj
@@ -16,4 +16,22 @@
   (is (= :off (get-light {} 5 8)))
   (let [lights (set-light {} 5 8 :on)]
     (is (= :off (get-light lights 4 2)))
-    (is (= :on (get-light lights 5 8)))))
+    (is (= :on (get-light lights 5 8))))
+  (let [lights (set-light {} 2 4 :toggle)
+        inverted (set-light {} 2 4 :toggle)]
+    (is (= :on (get-light lights 2 4))
+        (= :off (get-light inverted 2 4)))))
+
+(deftest rule-parsing
+  (is (= {:from {:x 0 :y 0}
+          :to {:x 999 :y 999}
+          :state :on}
+         (parse-rule "turn on 0,0 through 999,999")))
+  (is (= {:from {:x 0 :y 0}
+          :to {:x 999 :y 0}
+          :state :toggle}
+         (parse-rule "toggle 0,0 through 999,0")))
+  (is (= {:from {:x 499 :y 499}
+          :to {:x 500 :y 500}
+          :state :off}
+         (parse-rule "turn off 499,499 through 500,500"))))
