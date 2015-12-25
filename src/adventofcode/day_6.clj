230ab11e460a4d7e9aebc1b3d4b6c0c2235679e8
Oliver Caldwell <olliec87@gmail.com>
2015-12-25 20:05:03 +0000

    Add essential day 6 tool functions

--- src/adventofcode/day_6.clj
+++ src/adventofcode/day_6.clj
@@ -1,1 +1,20 @@
 (ns adventofcode.day-6)
+
+(def size 1000)
+
+(defn xy->i [x y]
+  (+ x (* size y)))
+
+(defn get-light [grid x y]
+  (get grid (xy->i x y) :off))
+
+(defn set-light [grid x y state]
+  (assoc grid (xy->i x y) state))
+
+(defn apply-rule [lights rule]
+  lights)
+
+(defn count-enabled-lights [input]
+  (let [lights (reduce apply-rule {} input)
+        light-nums (map {:on 1 :off 0} lights)]
+    (reduce-kv (fn [acc k v] (+ acc v)) 0 lights)))
--- test/adventofcode/day_6_test.clj
+++ test/adventofcode/day_6_test.clj
@@ -1,3 +1,19 @@
 (ns adventofcode.day-6-test
   (:require [clojure.test :refer :all]
-            [adventofcode.day-6 :refer :all]))
+            [adventofcode.day-6 :refer :all]
+            [clojure.java.io :as io]
+            [clojure.string :as string]))
+
+(def input (string/split (slurp (io/resource "inputs/day-6.txt")) #"\n"))
+
+(deftest day-1
+  (is (= nil (count-enabled-lights input))))
+
+(deftest grid-tools
+  (is (= 0 (xy->i 0 0)))
+  (is (= 10 (xy->i 10 0)))
+  (is (= 1000 (xy->i 0 1)))
+  (is (= :off (get-light {} 5 8)))
+  (let [lights (set-light {} 5 8 :on)]
+    (is (= :off (get-light lights 4 2)))
+    (is (= :on (get-light lights 5 8)))))
