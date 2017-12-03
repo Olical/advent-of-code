73a7894da18e90c6f54ac1ecea5299a77a9cfa28
Oliver Caldwell <olliec87@gmail.com>
2017-12-03 21:54:29 +0000

    Inefficient solution to day 3 part b, but I'm tired and on a train
    Honestly if it wasn't for Clojure I wouldn't have even got this far. Thanks,
    Rich et al.

--- 2017/src/advent/day_3.clj
+++ 2017/src/advent/day_3.clj
@@ -1,21 +1,37 @@
 (ns advent.day-3)
 
-(defn distance [target]
+(def neighbour-coords #{{:x 1} {:y -1} {:x -1} {:y 1}
+                       {:x 1, :y -1} {:x -1, :y -1}
+                       {:x -1, :y 1} {:x 1, :y 1}})
+
+(defn neighbour-sum [grid coord]
+  (let [rel-neighbours (map #(merge-with + coord %) neighbour-coords)
+        neighbour-vals (vals (select-keys grid rel-neighbours))]
+    (if (empty? neighbour-vals)
+      1M
+      (apply + neighbour-vals))))
+
+(defn spiral [target strategy]
   (loop [coord {:x 0, :y 0}
          n 1
          run-length 0
          turn-length (interleave (rest (range)) (rest (range)))
-         dirs (cycle [{:x 1} {:y -1} {:x -1} {:y 1}])]
-    (cond
-      (= n target) (+ (Math/abs (:x coord)) (Math/abs (:y coord)))
-      (= run-length
-         (first turn-length)) (recur coord
-                                     n
-                                     0
-                                     (rest turn-length)
-                                     (rest dirs))
-      :else (recur (merge-with + coord (first dirs))
-                   (inc n)
-                   (inc run-length)
-                   turn-length
-                   dirs))))
+         dirs (cycle [{:x 1} {:y -1} {:x -1} {:y 1}])
+         sum-grid {}]
+    (let [sum-grid (assoc sum-grid coord (neighbour-sum sum-grid coord))]
+      (cond
+        (and (= strategy :distance) (= n target)) (+ (Math/abs (:x coord)) (Math/abs (:y coord)))
+        (and (= strategy :sum-gt) (>= (sum-grid coord) target)) (sum-grid coord)
+        (= run-length
+           (first turn-length)) (recur coord
+           n
+           0
+           (rest turn-length)
+           (rest dirs)
+           sum-grid)
+        :else (recur (merge-with + coord (first dirs))
+                     (inc n)
+                     (inc run-length)
+                     turn-length
+                     dirs
+                     sum-grid)))))
--- 2017/test/advent/day_3_test.clj
+++ 2017/test/advent/day_3_test.clj
@@ -7,8 +7,11 @@
 (def input (edn/read-string (slurp (io/resource "day-3.txt"))))
 
 (t/deftest part-a
-  (t/is (= 0 (sut/distance 1)))
-  (t/is (= 3 (sut/distance 12)))
-  (t/is (= 2 (sut/distance 23)))
-  (t/is (= 31 (sut/distance 1024)))
-  (t/is (= 480 (sut/distance input))))
+  (t/is (= 0 (sut/spiral 1 :distance)))
+  (t/is (= 3 (sut/spiral 12 :distance)))
+  (t/is (= 2 (sut/spiral 23 :distance)))
+  (t/is (= 31 (sut/spiral 1024 :distance)))
+  (t/is (= 480 (sut/spiral input :distance))))
+
+(t/deftest part-b
+  (t/is (= 349975M (sut/spiral input :sum-gt))))
