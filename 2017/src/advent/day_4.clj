d7ed9601203c210103473bab31f990b8e1ead927
Oliver Caldwell <olliec87@gmail.com>
2017-12-04 11:25:44 +0000

    Day 4 part 1 done

--- 2017/src/advent/day_4.clj
+++ 2017/src/advent/day_4.clj
@@ -1,1 +1,12 @@
-(ns advent.day-4)
+(ns advent.day-4
+  (:require [clojure.string :as str]
+            [clojure.set :as set]))
+
+(defn parse [src]
+  (->> (str/split src #"\n")
+       (map #(str/split % #"\s+"))))
+
+(defn valid-phrase-count [src]
+  (->> (parse src)
+       (filter #(apply distinct? %))
+       (count)))
--- 2017/test/advent/day_4_test.clj
+++ 2017/test/advent/day_4_test.clj
@@ -1,6 +1,12 @@
 (ns advent.day-4-test
   (:require [advent.day-4 :as sut]
+            [clojure.java.io :as io]
             [clojure.test :as t]))
 
 (def input (slurp (io/resource "day-4.txt")))
 
+(t/deftest part-a
+  (t/is (= 1 (sut/valid-phrase-count "aa bb cc dd ee")))
+  (t/is (= 1 (sut/valid-phrase-count "aa bb cc dd aaa")))
+  (t/is (= 0 (sut/valid-phrase-count "aa bb cc dd aa")))
+  (t/is (= 451 (sut/valid-phrase-count input))))
