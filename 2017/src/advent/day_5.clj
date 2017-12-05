56c050ad7492ce74a6ac418ba79f87a7b5436db4
Oliver Caldwell <olliec87@gmail.com>
2017-12-05 11:29:24 +0000

    Day 5 part 2 complete

--- 2017/src/advent/day_5.clj
+++ 2017/src/advent/day_5.clj
@@ -6,12 +6,17 @@
   (->> (str/split src #"\n")
        (mapv edn/read-string)))
 
-(defn steps [src]
+(defn weird-inc [n]
+  (if (>= n 3)
+    (dec n)
+    (inc n)))
+
+(defn steps [src update-fn]
   (loop [instructions (parse src)
          pointer 0
          step 0]
     (if-let [instruction (get instructions pointer)]
-      (recur (update instructions pointer inc)
+      (recur (update instructions pointer update-fn)
              (+ pointer instruction)
              (inc step))
       step)))
--- 2017/test/advent/day_5_test.clj
+++ 2017/test/advent/day_5_test.clj
@@ -4,7 +4,12 @@
             [clojure.test :as t]))
 
 (def input (slurp (io/resource "day-5.txt")))
+(def example "0\n3\n0\n1\n-3")
 
 (t/deftest part-a
-  (t/is (= 5 (sut/steps "0\n3\n0\n1\n-3")))
-  (t/is (= 359348 (sut/steps input))))
+  (t/is (= 5 (sut/steps example inc)))
+  (t/is (= 359348 (sut/steps input inc))))
+
+(t/deftest part-b
+  (t/is (= 10 (sut/steps example sut/weird-inc)))
+  (t/is (= 27688760 (sut/steps input sut/weird-inc))))
