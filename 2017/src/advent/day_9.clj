4f61a9f628f512afb2b31b25d87e1b1c58c6079a
Oliver Caldwell <olliec87@gmail.com>
2017-12-09 17:40:25 +0000

    Day 9 part 1

--- 2017/src/advent/day_9.clj
+++ 2017/src/advent/day_9.clj
@@ -12,5 +12,24 @@
            :?))
        src))
 
-(defn score [src]
-  (parse src))
+(defn drop-until [target steps]
+  (drop-while #(not= target %) steps))
+
+(defn depths [src]
+  (loop [mode :normal
+         steps (parse src)
+         depths []
+         depth 1]
+    (if-let [[step & rest-steps] steps]
+      (if (= step :skip-next)
+        (recur mode (rest rest-steps) depths depth)
+        (case mode
+          :normal (case step
+                    :open-group (recur mode rest-steps (conj depths depth) (inc depth))
+                    :close-group (recur mode rest-steps depths (dec depth))
+                    :open-garbage (recur :garbage rest-steps depths depth)
+                    (recur mode rest-steps depths depth))
+          :garbage (case step
+                     :close-garbage (recur :normal rest-steps depths depth)
+                     (recur mode rest-steps depths depth))))
+      depths)))
