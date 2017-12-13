52b05b5368d3f8740b9ed33dd6870a99e112b3bd
Oliver Caldwell <olliec87@gmail.com>
2017-12-13 21:15:56 +0000

    Added basic parsing for day 12

--- 2017/src/advent/day_13.clj
+++ 2017/src/advent/day_13.clj
@@ -1,2 +1,15 @@
-(ns advent.day-13)
+(ns advent.day-13
+  (:require [clojure.string :as str]
+            [clojure.edn :as edn]))
 
+(defn parse [src]
+  (->> (str/split (str/trim src) #"\n")
+       (into {}
+             (comp
+              (map #(str/split % #": "))
+              (map (fn [[depth range]]
+                     [(edn/read-string depth)
+                      (edn/read-string range)]))))))
+
+(defn severity [firewall]
+  nil)
--- 2017/test/advent/day_13_test.clj
+++ 2017/test/advent/day_13_test.clj
@@ -2,3 +2,12 @@
   (:require [advent.day-13 :as sut]
             [clojure.test :as t]))
 
+(def example (sut/parse "0: 3
+1: 2
+4: 4
+6: 4"))
+
+example
+
+(t/deftest part-a
+  (t/is (= 24 (sut/severity example))))
