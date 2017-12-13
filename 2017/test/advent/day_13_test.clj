dd375237e58043c008886b326714df7811d4a31a
Oliver Caldwell <olliec87@gmail.com>
2017-12-13 21:21:49 +0000

    Included day 13 input and parsed it

--- 2017/test/advent/day_13_test.clj
+++ 2017/test/advent/day_13_test.clj
@@ -1,6 +1,9 @@
 (ns advent.day-13-test
   (:require [advent.day-13 :as sut]
-            [clojure.test :as t]))
+            [clojure.test :as t]
+            [clojure.java.io :as io]))
+
+(def input (sut/parse (slurp (io/resource "day-13.txt"))))
 
 (def example (sut/parse "0: 3
 1: 2
