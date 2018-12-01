6afd1eb3f726c88158a4d51270a56cc79304c3a3
Oliver Caldwell <olliec87@gmail.com>
2018-12-01 08:38:07 +0000

    Day 1 part 2

--- 2018/src/aoc/day_01.clj
+++ 2018/src/aoc/day_01.clj
@@ -9,9 +9,19 @@
          (map edn/read-string)
          (reduce +))))
 
+(defn first-dupe []
+  (with-open [rdr (io/reader "inputs/day-01.txt")]
+    (loop [freqs (->> (line-seq rdr) (map edn/read-string) (cycle))
+           sums #{0}
+           sum 0]
+      (let [sum (+ sum (first freqs))]
+        (if (contains? sums sum)
+          sum
+          (recur (rest freqs) (conj sums sum) sum))))))
+
 (t/deftest tests
   (t/testing "01 A"
     (t/is (= (sum-lines) 510)))
 
-  #_(t/testing "01 B"
-      (t/is (= (sum-lines) 0))))
+  (t/testing "01 B"
+    (t/is (= (first-dupe) 69074))))
