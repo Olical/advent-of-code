a6bfc213d0d21432ed9625fb63f29f299f192591
Oliver Caldwell <olliec87@gmail.com>
2018-12-01 08:15:17 +0000

    Solved day 1 a

--- 2018/src/aoc/day_01.clj
+++ 2018/src/aoc/day_01.clj
@@ -1,6 +1,19 @@
 (ns aoc.day-01
-  (:require [clojure.test :as t]))
+  (:require [clojure.test :as t]
+            [clojure.string :as str]
+            [clojure.edn :as edn]))
 
-(t/deftest part-1
-  (t/testing "it works"
-    (t/is (= :a :a))))
+(def input (slurp "inputs/day-01.txt"))
+
+(defn sum-lines [input]
+  (-> input
+      (str/split #"\n")
+      (->> (map edn/read-string)
+           (reduce +))))
+
+(t/deftest tests
+  (t/testing "01 A"
+    (t/is (= (sum-lines input) 510)))
+
+  #_(t/testing "01 B"
+      (t/is (= (sum-lines input) 0))))
