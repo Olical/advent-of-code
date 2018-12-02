de5673183669214a755431284ea7450dd44176ae
Oliver Caldwell <olliec87@gmail.com>
2018-12-02 15:20:50 +0000

    Day 2 part 2

--- 2018/src/aoc/day_02.clj
+++ 2018/src/aoc/day_02.clj
@@ -1,5 +1,6 @@
 (ns aoc.day-02
   (:require [clojure.test :as t]
+            [clojure.string :as str]
             [aoc.core :as aoc]))
 
 (defn checksum []
@@ -14,10 +15,31 @@
                     freqs)]
         (* (get dupes 2) (get dupes 3))))))
 
+(defn common-chars [as bs]
+  (reduce (fn [acc [a b]]
+            (if (= a b)
+              (conj acc a)
+              acc))
+          []
+          (map vector as bs)))
+
+(defn correct-id-common-chars []
+  (aoc/with-lines "day-02" identity
+    (fn [ids]
+      (some
+        (fn [a]
+          (some
+            (fn [b]
+              (let [cs (common-chars a b)]
+                (when (= (count cs) (dec (count a)))
+                  (str/join cs))))
+            ids))
+        ids))))
+
 (t/deftest day-02-a
   (t/testing "input"
     (t/is (= (checksum) 7105))))
 
 (t/deftest day-02-b
   (t/testing "input"
-    (t/is (= 0 0))))
+    (t/is (= (correct-id-common-chars) "omlvgdokxfncvqyersasjziup"))))
