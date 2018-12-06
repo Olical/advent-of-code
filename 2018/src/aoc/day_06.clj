f8e4418a42e9c689b0b1aea31e6d272b64ea257f
Oliver Caldwell <olliec87@gmail.com>
2018-12-06 20:18:54 +0000

    Day 6 part 1

--- 2018/src/aoc/day_06.clj
+++ 2018/src/aoc/day_06.clj
@@ -12,10 +12,10 @@
   (aoc/with-lines "day-06" parse-coord vec))
 
 (def bounds
-  {:from {:x (->> coords (map :x) (apply min) (- 15))
-          :y (->> coords (map :y) (apply min) (- 15))}
-   :to {:x (->> coords (map :x) (apply max) (+ 15))
-        :y (->> coords (map :y) (apply max) (+ 15))}})
+  {:from {:x (- (->> coords (map :x) (apply min)) 15)
+          :y (- (->> coords (map :y) (apply min)) 15)}
+   :to {:x (+ (->> coords (map :x) (apply max)) 15)
+        :y (+ (->> coords (map :y) (apply max)) 15)}})
 
 (def cells
   (for [x (range (-> bounds :from :x) (-> bounds :to :x inc))
@@ -24,17 +24,18 @@
      :y y}))
 
 (defn closest [coords {tx :x, ty :y}]
-  (let [[{:keys [coord distance]} & others]
+  (let [[a b]
         (->> coords
              (map
                (fn [{:keys [x y] :as coord}]
                  {:coord coord
                   :distance (+ (aoc/abs (- x tx))
                                (aoc/abs (- y ty)))}))
-             (sort-by :distance))]
-    (if (some #(= distance (:distance %)) others)
+             (sort-by :distance)
+             (take 2))]
+    (if (= (:distance a) (:distance b))
       :ambiguous
-      coord)))
+      (:coord a))))
 
 (defn edge? [{:keys [x y]}]
   (or (= x (-> bounds :from :x))
@@ -51,8 +52,9 @@
            (if cell
              (let [closest (closest coords cell)]
                (cond
+                 (= closest :ambiguous) (recur cells edges acc)
                  (edges closest) (recur cells edges acc)
-                 (edge? closest) (recur cells (assoc edges closest true) (dissoc acc closest))
+                 (edge? cell) (recur cells (assoc edges closest true) (dissoc acc closest))
                  :else (recur cells edges (update acc closest (fnil inc 0)))))
              acc))
          (vals)
