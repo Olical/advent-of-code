2f35d1186bf1981d321531f2005daf3c7fb80945
Oliver Caldwell <olliec87@gmail.com>
2018-12-07 19:00:34 +0000

    Completed day 7

--- 2018/src/aoc/day_07.clj
+++ 2018/src/aoc/day_07.clj
@@ -40,10 +40,67 @@
       (recur (disj steps step) (conj acc step))
       (->> acc (map name) (str/join)))))
 
+(def base-time 60)
+
+(def times
+  (->> (range 65 91)
+       (into {}
+             (map
+               (fn [n]
+                 [(-> n char str keyword) (+ n -64 base-time)])))))
+
+(defn advance [steps workers done]
+  (loop [steps steps
+         workers workers
+         done done
+         i (dec (count workers))]
+    (if (>= i 0)
+      (let [worker (get workers i)
+            step (available done steps)]
+        (cond
+          ;; Assign work.
+          (and (= worker :ready) step)
+          (recur (disj steps step)
+                 (assoc workers i [step (get times step)])
+                 done
+                 (dec i))
+
+          ;; Do nothing. No work left.
+          (and (= worker :ready) (not step))
+          (recur steps
+                 workers
+                 done
+                 (dec i))
+
+          ;; Move work to done, set to ready and re-attempt.
+          (<= (second worker) 1)
+          (recur steps
+                 (assoc workers i :ready)
+                 (conj done (first worker))
+                 i)
+
+          ;; Just decrement the work remaining.
+          :else
+          (recur steps
+                 (update-in workers [i 1] dec)
+                 done
+                 (dec i))))
+      [steps workers done])))
+
+(defn parallel-time []
+  (loop [steps steps
+         workers [:ready :ready :ready :ready :ready]
+         done #{}
+         tick 0]
+    (if (or (seq steps) (apply not= :ready workers))
+      (let [[steps workers done] (advance steps workers done)]
+        (recur steps workers done (inc tick)))
+      (dec tick))))
+
 (t/deftest day-07-a
   (t/testing "input"
     (t/is (= (step-string) "GJKLDFNPTMQXIYHUVREOZSAWCB"))))
 
 (t/deftest day-07-b
   (t/testing "input"
-    (t/is (= 0 0))))
+    (t/is (= (parallel-time) 967))))
