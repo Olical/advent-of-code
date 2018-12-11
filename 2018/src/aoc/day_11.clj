ad7890a55546a289295bed3c6fcc44b84752cf10
Oliver Caldwell <olliec87@gmail.com>
2018-12-11 09:02:25 +0000

    Add templates for day 11

---
+++ 2018/src/aoc/day_11.clj
@@ -0,0 +1,13 @@
+(ns aoc.day-11
+  (:require [clojure.test :as t]
+            [aoc.core :as aoc]))
+
+(t/deftest day-11-a
+  (t/testing "input"
+    (t/is (= 0 0))))
+
+(t/deftest day-11-b
+  (t/testing "input"
+    (t/is (= 0 0))))
+
+(def grid-serial 9110)
