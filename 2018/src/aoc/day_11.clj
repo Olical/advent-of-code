b00fd4d42575bf655c00171bd53877624456c4fa
Oliver Caldwell <olliec87@gmail.com>
2018-12-11 09:14:35 +0000

    Starting the power fn

--- 2018/src/aoc/day_11.clj
+++ 2018/src/aoc/day_11.clj
@@ -10,4 +10,19 @@
   (t/testing "input"
     (t/is (= 0 0))))
 
-(def grid-serial 9110)
+(def grid-serial 57 #_9110)
+
+; (for [x (range 1 301)
+;       y (range 1 301)])
+
+(defn power [x y]
+  (let [rack (+ x 10)]
+    (-> (* rack y)
+        (+ grid-serial)
+        (* rack)
+        (/ 100)
+        (mod 10)
+        (- 5)
+        (int))))
+
+(power 122 79)
