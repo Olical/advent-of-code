bdaff72c79456815dcd0bdbb2f756649f824cd79
Oliver Caldwell <olliec87@gmail.com>
2018-12-12 13:08:06 +0000

    Day 12 part 1

--- 2018/src/aoc/day_12.clj
+++ 2018/src/aoc/day_12.clj
@@ -3,37 +3,61 @@
             [clojure.string :as str]
             [aoc.core :as aoc]))
 
+(defn initial-state [s]
+  (zipmap
+    (range)
+    (map (comp keyword str) (str/replace s #"initial state: " ""))))
+
+(defn rule-fns [rules]
+  (keep
+    (fn [s]
+      (let [[ll l cur r rr res] (map (comp keyword str) (str/replace s #" => " ""))]
+        (when (not= res :.)
+          (fn [pots n]
+            (when (and (= (get pots n :.) cur)
+
+                       (= (get pots (- n 2) :.) ll)
+                       (= (get pots (- n 1) :.) l)
+
+                       (= (get pots (+ n 1) :.) r)
+                       (= (get pots (+ n 2) :.) rr))
+              res)))))
+    rules))
+
+(def input
+  (aoc/with-input "day-12"
+    (fn [f]
+      (let [[state _ & rules] (str/split-lines (slurp f))]
+        {:state (initial-state state)
+         :rule-fns (rule-fns rules)}))))
+
+(defn gen-20 []
+  (->>
+    (reduce
+      (fn [state _]
+        (into
+          {}
+          (map
+            (fn [n]
+              [n
+               (or
+                 (some
+                   (fn [f]
+                     (f state n))
+                   (:rule-fns input))
+                 :.)]))
+          (range -1000 1000)))
+      (:state input)
+      (range 20))
+    (filter #(= (val %) :#))
+    (map key)
+    (reduce +)))
+
 (t/deftest day-12-a
   (t/testing "input"
-    (t/is (= 0 0))))
+    (t/is (= (gen-20) 3793))))
 
 (t/deftest day-12-b
   (t/testing "input"
     (t/is (= 0 0))))
 
-(defn initial-state [s]
-  (map (comp keyword str) (str/replace s #"initial state: " "")))
-
-(defn rule-fns [rules]
-  (keep
-    (fn [s]
-      (let [[ll l c r rr n] (map (comp keyword str) (str/replace s #" => " ""))]
-        (if (= c n)
-          nth
-          (fn [pots n]
-            (if (and (= (nth pots n) c)
-
-                     (= (nth pots (- n 2)) ll)
-                     (= (nth pots (- n 1)) l)
-
-                     (= (nth pots (+ n 1)) r)
-                     (= (nth pots (+ n 2)) rr))
-              n
-              c)))))
-    rules))
-
-(aoc/with-input "day-12"
-  (fn [f]
-    (let [[state _ & rules] (str/split-lines (slurp f))]
-      {:state (initial-state state)
-       :rule-fns (rule-fns rules)})))
