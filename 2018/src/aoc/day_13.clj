755e74ed21cd21ed070d3c13ceef1943bcd9f5a7
Oliver Caldwell <olliec87@gmail.com>
2018-12-13 22:39:10 +0000

    Added the day 13 template

---
+++ 2018/src/aoc/day_13.clj
@@ -0,0 +1,11 @@
+(ns aoc.day-13
+  (:require [clojure.test :as t]
+            [aoc.core :as aoc]))
+
+(t/deftest day-13-a
+  (t/testing "input"
+    (t/is (= 0 0))))
+
+(t/deftest day-13-b
+  (t/testing "input"
+    (t/is (= 0 0))))
