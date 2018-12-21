c3bf112a005305dce9750f42531ec1b9c15f53a7
Oliver Caldwell <olliec87@gmail.com>
2018-12-21 10:25:05 +0000

    Started day 13, not going to finish :(

--- 2018/src/aoc/day_13.clj
+++ 2018/src/aoc/day_13.clj
@@ -9,3 +9,19 @@
 (t/deftest day-13-b
   (t/testing "input"
     (t/is (= 0 0))))
+
+(defn char->tile [c]
+  (case c
+    \| [:track :vertical]
+    \- [:track :horizontal]
+    \/ [:track :BL->TR]
+    \\ [:track :TL->BR]
+    \+ [:track :intersection]
+    \> [:cart :right 0]
+    \v [:cart :down 0]
+    \< [:cart :left 0]
+    \^ [:cart :up 0]
+    [:space]))
+
+(def grid
+  (aoc/with-lines "day-13" #(mapv char->tile %) vec))
