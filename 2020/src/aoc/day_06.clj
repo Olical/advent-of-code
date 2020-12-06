c8e46c6dba0ef1cf0ac0fdc270c2493119864c3d
Oliver Caldwell <ollie@oli.me.uk>
2020-12-06 12:03:20 +0000

    Day 6 part b

--- 2020/src/aoc/day_06.clj
+++ 2020/src/aoc/day_06.clj
@@ -1,21 +1,30 @@
 (ns aoc.day-06
   (:require [clojure.test :as t]
             [clojure.string :as str]
+            [clojure.set :as set]
             [aoc.core :as aoc]))
 
 (def input (aoc/with-input "day-06"
              (fn [reader]
-               (map str/split-lines (str/split (slurp reader) #"\n\n")))))
+               (map #(map set (str/split-lines %))
+                    (str/split (slurp reader) #"\n\n")))))
 
 (defn part-a [groups]
   (->> groups
        (map
          (fn [group]
-           (count (reduce into (map set group)))))
+           (count (reduce into group))))
+       (reduce +)))
+
+(defn part-b [groups]
+  (->> groups
+       (map
+         (fn [group]
+           (count (apply set/intersection group))))
        (reduce +)))
 
 (t/deftest day-06-a
-  (t/is (= 0 (part-a input))))
+  (t/is (= 6297 (part-a input))))
 
 (t/deftest day-06-b
-  (t/is (= 0 0)))
+  (t/is (= 0 (part-b input))))
