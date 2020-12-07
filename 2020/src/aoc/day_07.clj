0502b4084f0aecedff286d3674f41aae7fdc239f
Oliver Caldwell <oliver.caldwell@viasat.com>
2020-12-07 22:38:52 +0000

    Day 7, part 1

--- 2020/src/aoc/day_07.clj
+++ 2020/src/aoc/day_07.clj
@@ -21,7 +21,37 @@
      :colour colour
      :children children}))
 
-(def input (aoc/with-lines "day-07" parse vec))
+(defn parent? [child-id maybe-parent]
+  (some
+    (fn [child]
+      (= child-id (bag->id child)))
+    (:children maybe-parent)))
+
+(defn direct-parents [bags target-id]
+  (vec
+    (keep
+      (fn [bag]
+        (when (parent? target-id bag)
+          (bag->id bag)))
+      bags)))
+
+(defn index [bags]
+  (->> bags
+       (map
+         (fn [bag]
+           (let [bag-id (bag->id bag)]
+             [bag-id
+              (assoc bag :parents (direct-parents bags bag-id))])))
+       (into {})))
+
+(def bags (aoc/with-lines "day-07" parse index))
+
+(defn all-parents [target-id]
+  (let [{:keys [parents]} (get bags target-id)]
+    (concat parents (mapcat all-parents parents))))
+
+(defn root? [bag-id]
+  (empty? (get bags [bag-id :parents])))
 
 (t/deftest day-07-a
   (t/is (= {:desc "bright"
@@ -34,7 +64,12 @@
                        {:amount 6, :desc "dotted", :colour "black"}]}
            (parse "vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.")))
   (t/is (= {:desc "faded", :colour "blue", :children nil}
-           (parse "faded blue bags contain no other bags."))))
+           (parse "faded blue bags contain no other bags.")))
+  (t/is (= 213
+           (->> (all-parents {:desc "shiny", :colour "gold"})
+                (filter root?)
+                (distinct)
+                (count)))))
 
 (t/deftest day-07-b
   (t/is (= 0 0)))
