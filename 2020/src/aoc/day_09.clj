c85f70fd4153df2f38e10e5af34fb9a845aae7f8
Oliver Caldwell <oliver.caldwell@viasat.com>
2020-12-09 10:44:06 +0000

    Day 9 complete

--- 2020/src/aoc/day_09.clj
+++ 2020/src/aoc/day_09.clj
@@ -6,7 +6,7 @@
 
 (def input (aoc/with-lines "day-09" edn/read-string vec))
 
-(defn find-weakness [n xs]
+(defn weakness-precursor [n xs]
   (loop [prev (vec (take n xs))
          xs (drop n xs)]
     (let [[x & xs] xs
@@ -17,8 +17,22 @@
           xs)
         x))))
 
+(defn weakness [target xs]
+  (loop [sum (first xs)
+         window [(first xs)]
+         xs (rest xs)]
+    (cond
+      (= sum target) (let [result (sort window)]
+                       (+ (first result) (last result)))
+      (> sum target) (recur (- sum (first window))
+                            (subvec window 1)
+                            xs)
+      (< sum target) (recur (+ sum (first xs))
+                            (conj window (first xs))
+                            (rest xs)))))
+
 (t/deftest day-09-a
-  (t/is (= 373803594 (find-weakness 25 input))))
+  (t/is (= 373803594 (weakness-precursor 25 input))))
 
 (t/deftest day-09-b
-  (t/is (= 0 0)))
+  (t/is (= 51152360 (weakness 373803594 input))))
