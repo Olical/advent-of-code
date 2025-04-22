488afe2b93104008bd7fd30d79cbda6119a69d82
Oliver Caldwell <ollie@oli.me.uk>
2025-04-22 23:01:36 +0100

    2024 01 part 1

--- 2024/src/aoc/day_01.clj
+++ 2024/src/aoc/day_01.clj
@@ -1,4 +1,24 @@
 (ns aoc.day-01
-  (:require [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]
+            [malli.experimental :as mx]))
 
 (def input-resource (io/resource "aoc/day_01.txt"))
+
+(mx/defn parse-row :- [:sequential pos-int?]
+  [row :- string?]
+  (map parse-long (str/split row #"\s+")))
+
+(mx/defn difference :- int?
+  [a :- pos-int?
+   b :- pos-int?]
+  (Math/abs (- a b)))
+
+(mx/defn part-1 :- pos-int?
+  [input]
+  (let [lines (line-seq (io/reader input))
+        pairs (map parse-row lines)
+        left (sort (map first pairs))
+        right (sort (map second pairs))
+        differences (map difference left right)]
+    (reduce + differences)))
--- 2024/test/aoc/day_01_test.clj
+++ 2024/test/aoc/day_01_test.clj
@@ -5,3 +5,13 @@
 
 (t/deftest input-resource
   (t/is (not (str/blank? (slurp day-01/input-resource)))))
+
+(t/deftest parse-row
+  (t/is (= '(80414 72092) (day-01/parse-row "80414   72092"))))
+
+(t/deftest difference
+  (t/is (= 5 (day-01/difference 10 5)))
+  (t/is (= 5 (day-01/difference 5 10))))
+
+(t/deftest part-1
+  (t/is (= 1646452 (day-01/part-1 day-01/input-resource))))
